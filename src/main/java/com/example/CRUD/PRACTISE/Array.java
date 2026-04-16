59bf8d6f8798a80ed0422e508ec6c67be800b8d7
Akash <tyagiakash577@gmail.com>
2026-04-16 10:47:51 +0530

    Map Collection IntroDuction

--- src/main/java/com/example/CRUD/PRACTISE/Array.java
+++ src/main/java/com/example/CRUD/PRACTISE/Array.java
@@ -3,26 +3,12 @@ import java.util.*;
 
 public class Array {
     public static void main(String[] args) {
-//        int[] arr={1,2,3,2,1,4,5};
-//        Set<Integer> set = new HashSet<>();
-//        for (int num:arr){
-//            set.add(num);
-//        }
-//        System.out.println(Arrays.toString(arr)+" result "+set);
-//        List<Integer> list= new ArrayList<>();
-//        for (int num:arr){
-//            if (!list.contains(num)){
-//                list.add(num);
-//            }
-//        }
-//        System.out.println(list);
-
         List<Employee> list= new ArrayList<>();
         list.add(new Employee(1,"Akash",34400));
         list.add(new Employee(2,"Ravi",50000));
-        list.add(new Employee(3,"Rajesh",1000));
-
-        Map<Integer,Employee>map= new HashMap<>();
+        list.add(new Employee(3,"Rohan",1000));
+        System.out.println("HIIIII "+list);
+        Map<Integer,Employee> map= new HashMap<>();
         for (Employee emp:list){
             if (emp.salary>5000){
                 map.put(emp.id,emp);
@@ -32,10 +18,7 @@ public class Array {
             Employee e= entry.getValue();
             System.out.println(e.name+" "+e.salary);
         }
-
     }
-
-
 }
 
 class Employee {
