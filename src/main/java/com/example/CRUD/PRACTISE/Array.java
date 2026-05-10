4e5b0eaea6e584fa0830f8b946803c9db991f609
Akash <tyagiakash577@gmail.com>
2026-05-10 22:36:26 +0530

    Java Streams

--- src/main/java/com/example/CRUD/PRACTISE/Array.java
+++ src/main/java/com/example/CRUD/PRACTISE/Array.java
@@ -1,5 +1,6 @@
 package com.example.CRUD.PRACTISE;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Array {
     public static void main(String[] args) {
@@ -7,17 +8,23 @@ public class Array {
         list.add(new Employee(1,"Akash",34400));
         list.add(new Employee(2,"Ravi",50000));
         list.add(new Employee(3,"Rohan",1000));
-        System.out.println("HIIIII "+list.toString());
-        Map<Integer,Employee> map= new HashMap<>();
-        for (Employee emp:list){
-            if (emp.salary>5000){
-                map.put(emp.id,emp);
-            }
-        }
-        for (Map.Entry<Integer,Employee> entry:map.entrySet()){
-            Employee e= entry.getValue();
-            System.out.println(e.name+" "+e.salary);
-        }
+//        System.out.println("HIIIII "+list.toString());
+//        Map<Integer,Employee> map= new HashMap<>();
+//        for (Employee emp:list){
+//            if (emp.salary>5000){
+//                map.put(emp.id,emp);
+//            }
+//        }
+//        for (Map.Entry<Integer,Employee> entry:map.entrySet()){
+//            Employee e= entry.getValue();
+//            System.out.println(e.name+" "+e.salary);
+//        }
+
+        Map<Integer ,Employee> map= list.stream()
+                .filter(employee -> employee.salary>5000)
+                .collect(Collectors.toMap(employee -> employee.id,employee -> employee));
+        map.values()
+                .forEach(employee -> System.out.println(employee.name + " "+ employee.salary));
     }
 }
 
