1749ba6cc5f51fd5da76a38c8df01437a556f204
Akash <tyagiakash577@gmail.com>
2026-07-28 21:24:44 +0530

    commit

--- src/main/java/com/example/CRUD/PRACTISE/Demo.java
+++ src/main/java/com/example/CRUD/PRACTISE/Demo.java
@@ -1,5 +1,7 @@
 package com.example.CRUD.PRACTISE;
 
+import java.util.*;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Demo");
@@ -11,21 +13,6 @@ public class Demo {
         }
 
         System.out.println("Result "+result);
-    }
-}
-
-class palindrome{
-    public static void main(String[] args) {
-        String pal="aahaa";
-        boolean isPal=true;
-        for (int i =0; i<pal.length()/2 ; i++){
-            if (pal.charAt(i) != pal.charAt(pal.length()-1-i)){
-                isPal=false;
-                break;
-            }
-        }
-        System.out.println("Result of palindrome "+ isPal);
-
     }
 }
 
@@ -49,7 +36,7 @@ class secondlargestnumber{
 class frequency{
 
     public static void main(String[] args) {
-        String name ="Akash ";
+        String name ="Test ";
         int[] freq=new int[256];
 
         for (int i =0;i<name.length();i++){
@@ -72,7 +59,6 @@ class frequency{
 
 }
 
-
 abstract class abstractone{
     int num=11;
 
@@ -84,3 +70,139 @@ interface diff{
     void test();
 }
 
+class reverse {
+    public static void main(String[] args) {
+        String name = "test";
+        for(int i=name.length()-1;i>=0;i--){
+            System.out.print(name.charAt(i));
+        }
+    }
+}
+
+class palindrome{
+    public static void main(String[] args) {
+        String val="level";
+        boolean isPal=true;
+        for (int i=0; i<val.length()/2; i++){
+            if (val.charAt(i) != val.charAt(val.length() - 1 - i)) {
+                isPal = false;
+                break;
+            }
+        }
+        System.out.println(isPal);
+    }
+}
+
+class Anagram{
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+        boolean isAng=true;
+        if (str1.length()!=str1.length()){
+            isAng=false;
+            return;
+        }else{
+            char[] a=str1.toCharArray();
+            char[] b=str2.toCharArray();
+            java.util.Arrays.sort(a);
+            java.util.Arrays.sort(b);
+            if (!java.util.Arrays.equals(a,b)){
+                isAng=false;
+            }
+        }
+        System.out.println(isAng);
+    }
+}
+
+class charCount{
+    public static void main(String[] args) {
+        String str1="Testing data";
+        int[] freq= new int[256];
+        for (int i=0; i<str1.length(); i++){
+            if (str1.charAt(i)==' '){
+                continue;
+            }
+            freq[str1.charAt(i)]++;
+        }
+        for (int i=0; i<freq.length;i++){
+            if (freq[i]>0){
+                char ch=(char) i;
+                System.out.println(ch+"  "+freq[i]);
+            }
+        }
+    }
+}
+
+class duplicatecharacter {
+    public static void main(String[] args) {
+        String string="hello";
+        for (int i = 0; i < string.length(); i++) {
+            for (int j = i+1; j < string.length(); j++) {
+                if (string.charAt(i)==string.charAt(j)){
+                    System.out.println("Duplicate character  "+string.charAt(i));
+                    break;
+                }
+            }
+        }
+    }
+}
+
+class RemoveDuplicates {
+    public static void main(String[] args) {
+        String string = "hello";
+        String result = "";
+        for (int i = 0; i < string.length(); i++) {
+            boolean duplicate = false;
+            for (int j = 0; j < result.length(); j++) {
+                if (string.charAt(i) == result.charAt(j)) {
+                    duplicate = true;
+                    break;
+                }
+            }
+            if (!duplicate) {
+                result = result + string.charAt(i);
+            }
+        }
+        System.out.println("Original String: " + string);
+        System.out.println("After removing duplicates: " + result);
+    }
+}
+
+class TopTenDifferenceQuestions{
+    public static void main(String[] args) {
+
+        ArrayList<Integer> arrayList= new ArrayList<>(2);
+        arrayList.add(1);
+        arrayList.add(12);
+        arrayList.add(123);
+        arrayList.add(100);
+        System.out.println(arrayList);
+
+        LinkedList<Integer> list= new LinkedList<>();
+        list.add(12);
+        list.addLast(1);
+        list.addFirst(11);
+        System.out.println(list);
+
+        //HashMap, LinkedHashMap, and TreeMap
+
+        Map<Integer,String> map = new HashMap<>();
+        map.put(3,"c");
+        map.put(1,null);
+        map.put(null,null);
+        System.out.println(map);
+
+        Map<Integer,String> map1 = new LinkedHashMap<>();
+        map1.put(3,"c");
+        map1.put(1,null);
+        map1.put(null,null);
+        System.out.println(map1);
+
+        Map<Integer,String> map2 = new TreeMap<>();
+        map2.put(3,"c");
+        map2.put(1,null);
+        System.out.println(map2);
+
+
+    }
+}
