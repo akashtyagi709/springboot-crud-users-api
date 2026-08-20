068d86f8e8e862f568d91352a7b726eb95b41f8a
Akash <tyagiakash577@gmail.com>
2026-08-20 21:47:28 +0530

    commit

--- src/main/java/com/example/CRUD/PRACTISE/LastPrep.java
+++ src/main/java/com/example/CRUD/PRACTISE/LastPrep.java
@@ -87,7 +87,61 @@ public class LastPrep {
         String s2 = "silent";
         int[] freq1 = new int [256];
         int[] freq2 = new int [256];
+        if (s1.length()!=s2.length()){
+            System.out.println("Not Anangram");
+            return;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            freq1[s1.charAt(i)]++;
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            freq2[s2.charAt(i)]++;
+        }
 
+        boolean isAnagram =true;
+        for (int i = 0; i < freq1.length; i++) {
+            if (freq1[i]!=freq2[i]){
+                isAnagram=false;
+                break;
+            }
+        }
+        if (isAnagram){
+            System.out.println("Yes it's an Anangram");
+        }else {
+            System.out.println("Not an Anagram");
+        }
 
+        int[] removeDuplicates = {1,2,2,3,4,4,5};
+        Set set1 = new HashSet();
+        for (int i = 0; i < removeDuplicates.length; i++) {
+                set1.add(removeDuplicates[i]);
+        }
+
+        System.out.println("removed duplicated " +set1);
+        int[] moveZero={1,0,2,0,3,4,0};
+        for (int i = 0; i < moveZero.length; i++) {
+            for (int j = i+1; j < moveZero.length; j++) {
+                if (moveZero[i]==0 & moveZero[j]!=0){
+                        int temp = moveZero[i];
+                        moveZero[i] = moveZero[j];
+                        moveZero[j] = temp;
+                }
+                }
+
+        }
+        System.out.println("First " + Arrays.toString(moveZero));
+        int[] sortArray = {4,2,8,1,9};
+        for (int i = 0; i < sortArray.length; i++) {
+            for (int j = i+1; j < sortArray.length; j++) {
+                if (sortArray[i]<sortArray[j]){
+                    int temp = sortArray[i];
+                    sortArray[i]=sortArray[j];
+                    sortArray[j]=temp;
+                }
+            }
+        }
+
+        System.out.println( "Largest "+sortArray[0]);
+        System.out.println( "Smallest  "+sortArray[sortArray.length-1]);
     }
 }
